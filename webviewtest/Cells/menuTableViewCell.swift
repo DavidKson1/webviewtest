d2b8ec77f6c5f45678abfe01a72c901221d36ff7
NickeGilbert <nicklas.gilbertson@gmail.com>
2018-08-01 00:43:26 +0200

    sixth commit
    Added icon

---
+++ webviewtest/Cells/menuTableViewCell.swift
@@ -0,0 +1,15 @@
+//
+//  menuTableViewCell.swift
+//  webviewtest
+//
+//  Created by Nicklas Gilbertsson on 2018-08-01.
+//  Copyright © 2018 David Karlsson. All rights reserved.
+//
+
+import UIKit
+
+class menuTableViewCell: UITableViewCell {
+
+    @IBOutlet weak var myLabel: UILabel!
+    
+}
--- webviewtest/Controllers/MenuViewController.swift
+++ webviewtest/Controllers/MenuViewController.swift
@@ -8,14 +8,32 @@
 
 import UIKit
 
-class MenuViewController: UIViewController {
+class MenuViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
 
+    @IBOutlet weak var tableView: UITableView!
+    
+    
+    let array = ["Om Oss", "Grafisk Design", "Broschyr/Säljblad", "Logo/Visitkort", "Hemsida", "App", "Marknadsföring", "Berätta Om Ditt Projekt"]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        tableView.delegate = self
+        tableView.dataSource = self
 
         self.revealViewController().rearViewRevealWidth = self.view.frame.size.width - 60
     }
 
-
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return array.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: "cell") as? menuTableViewCell else { return UITableViewCell() }
+        
+        cell.myLabel.text = array[indexPath.row]
+        
+        return cell
+    }
 
 }
