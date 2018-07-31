b82ea78ae9828492aa206364e9e5bb1886092baf
NickeGilbert <nicklas.gilbertson@gmail.com>
2018-08-01 00:19:25 +0200

    forth commit
    added email

--- webviewtest/Controllers/ViewController.swift
+++ webviewtest/Controllers/ViewController.swift
@@ -12,28 +12,45 @@ import WebKit
 class ViewController: UIViewController {
 
     @IBOutlet weak var webViewImage: UIImageView!
+    @IBOutlet weak var contactImage: UIImageView!
     @IBOutlet weak var menuBtn: UIButton!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        let tapGesture = UITapGestureRecognizer(target: self, action: #selector(ViewController.imageTapped(gesture:)))
-        webViewImage.addGestureRecognizer(tapGesture)
+        let WebviewtapGesture = UITapGestureRecognizer(target: self, action: #selector(ViewController.webViewImageTapped(gesture:)))
+        let contactTapGesture = UITapGestureRecognizer(target: self, action: #selector(ViewController.contactImageTapped(gesture:)))
+        
+        webViewImage.addGestureRecognizer(WebviewtapGesture)
+        contactImage.addGestureRecognizer(contactTapGesture)
         webViewImage.isUserInteractionEnabled = true
+        contactImage.isUserInteractionEnabled = true
+        
+        
         
         menuBtn.addTarget(self.revealViewController(), action: #selector(SWRevealViewController.revealToggle(_:)), for: .touchUpInside)
-      
         self.view.addGestureRecognizer(self.revealViewController().panGestureRecognizer())
         self.view.addGestureRecognizer(self.revealViewController().tapGestureRecognizer())
     }
 
-    @objc func imageTapped(gesture: UIGestureRecognizer) {
+    @objc func webViewImageTapped(gesture: UIGestureRecognizer) {
         if (gesture.view as? UIImageView) != nil {
             performSegue(withIdentifier: "dataDavid", sender: nil)
         }
     }
-
-
+    
+    @objc func contactImageTapped(gesture: UIGestureRecognizer) {
+        if (gesture.view as? UIImageView) != nil {
+            let email = "info@datadavid.se"
+            if let url = URL(string: "mailto:\(email)") {
+                if #available(iOS 10.0, *) {
+                    UIApplication.shared.open(url)
+                } else {
+                    UIApplication.shared.openURL(url)
+                }
+            }
+        }
+    }
 }
 
 
