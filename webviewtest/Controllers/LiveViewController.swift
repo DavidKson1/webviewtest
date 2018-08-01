6abfd27fcbdb7f932c92bc0aef3ddd77a8d6979d
NickeGilbert <nicklas.gilbertson@gmail.com>
2018-08-01 13:00:43 +0200

    added new icons and launch screen

---
+++ webviewtest/Controllers/LiveViewController.swift
@@ -0,0 +1,23 @@
+//
+//  LiveViewController.swift
+//  webviewtest
+//
+//  Created by Nicklas Gilbertsson on 2018-08-01.
+//  Copyright © 2018 David Karlsson. All rights reserved.
+//
+
+import UIKit
+
+class LiveViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+     
+    }
+
+    @IBAction func backBtnWasPressed(_ sender: Any) {
+        dismiss(animated: true, completion: nil)
+    }
+    
+}
